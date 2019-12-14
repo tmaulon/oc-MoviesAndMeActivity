d1ad878f5187c4ed9ccdf7c9785a3d99b5324bce
tmaulon <thomas.maulon@gmail.com>
2019-12-14 13:31:26 +0100

    improve stackNavigator design and refactor code

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -13,52 +13,56 @@ import Favorites from "../Components/Favorites";
 import News from "../Components/News";
 import Seen from "../Components/Seen";
 
-const SearchStackNavigator = createStackNavigator({
-  Search: {
-    screen: Search,
-    navigationOptions: {
-      title: "Movies And Me"
-    }
-  },
-  FilmDetail: {
-    screen: FilmDetail
+const FilmDetailNavigation = {
+  screen: FilmDetail,
+  navigationOptions: {
+    title: "En savoir plus",
+    headerStyle: {
+      backgroundColor: "#fff",
+    },
+    headerTintColor: '#0000ff',
+    headerTitleStyle: {
+      fontSize: 16,
+      color: "#0000ff",
+      fontWeight: "bold",
+    },
   }
+}
+const HeaderNavigation = (screenName, navigationOptionsTitle) => (
+  {
+    screen: screenName,
+    navigationOptions: {
+      title: navigationOptionsTitle,
+      headerStyle: {
+        backgroundColor: "#0000ff",
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontSize: 20,
+        color: "#fff",
+        fontWeight: "bold",
+      },
+    }
+  }
+);
+const SearchStackNavigator = createStackNavigator({
+  Search: HeaderNavigation(Search, "Movies and Me"),
+  FilmDetail: FilmDetailNavigation,
 });
 
 const FavoritesStackNavigator = createStackNavigator({
-  Favorites: {
-    screen: Favorites,
-    navigationOptions: {
-      title: "Favoris"
-    }
-  },
-  FilmDetail: {
-    screen: FilmDetail
-  }
+  Favorites: HeaderNavigation(Favorites, "Mes Films Favoris"),
+  FilmDetail: FilmDetailNavigation,
 });
 
 const NewsStackNavigator = createStackNavigator({
-  News: {
-    screen: News,
-    navigationOptions: {
-      title: "Les Derniers Films"
-    }
-  },
-  FilmDetail: {
-    screen: FilmDetail
-  }
+  News: HeaderNavigation(News, "Les Derniers Films"),
+  FilmDetail: FilmDetailNavigation,
 });
 
 const SeenStackNavigator = createStackNavigator({
-  Seen: {
-    screen: Seen,
-    navigationOptions: {
-      title: "Mes Films Vus"
-    }
-  },
-  FilmDetail: {
-    screen: FilmDetail
-  }
+  Seen: HeaderNavigation(Seen, "Mes Films Vus"),
+  FilmDetail: FilmDetailNavigation,
 });
 
 const MoviesTabNavigator = createBottomTabNavigator(
@@ -130,6 +134,16 @@ const styles = StyleSheet.create({
   icon: {
     width: 30,
     height: 30
+  },
+  stackNavigator: {
+    fontSize: 20,
+    fontWeight: "700",
+    color: "#fff",
+    backgroundColor: "#0000ff",
+    flex: 1,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center"
   }
 });
 
